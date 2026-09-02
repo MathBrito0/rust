8af90b6ab53966b7908ddded1c4970b815683106
CooperFutureDev <matheusbrito2005@gmail.com>
2026-09-02 17:11:29 -0300

    Ignora arquivos gerados pelo Cargo

--- target/debug/deps/exercicio_beta-98493da39c8c4ac9.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rust/target/debug/deps/exercicio_beta-98493da39c8c4ac9.d: exercicio-beta/src/main.rs
-
-/workspaces/rust/target/debug/deps/exercicio_beta-98493da39c8c4ac9: exercicio-beta/src/main.rs
-
-exercicio-beta/src/main.rs:
